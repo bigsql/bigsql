793c21fc8cfaeec4a3dbfdd03dda2375ffdd89f9
Denis Lussier <denis@lussier.io>
2019-12-18 06:29:51 -0800

    fix lib bloat

--- devel/pgbin/build/sharedLibs.sh
+++ devel/pgbin/build/sharedLibs.sh
@@ -7,20 +7,23 @@ else
   lib64=/usr/lib64
 fi
 
-cp -v $lib64/libreadline* $shared_lib/.
-cp -v $lib64/libtermcap*  $shared_lib/.
-cp -v $lib64/libz*        $shared_lib/.
-cp -v $lib64/libssl*      $shared_lib/.
-cp -v $lib64/lib*crypto*  $shared_lib/.
-cp -v $lib64/libkrb5*     $shared_lib/.
-cp -v $lib64/libcom_err*  $shared_lib/.
-cp -v $lib64/libgssapi*   $shared_lib/.
-cp -v $lib64/libxslt*     $shared_lib/.
-cp -v $lib64/libldap*     $shared_lib/.
-cp -v $lib64/liblber*     $shared_lib/.
-cp -v $lib64/libsasl2*    $shared_lib/.
-cp -v $lib64/libuuid*     $shared_lib/.
-cp -v $lib64/libxml2*     $shared_lib/.
-cp -v $lib64/libevent*    $shared_lib/.
+rm -f $shared_lib/*
+
+cp -v $lib64/libreadline*    $shared_lib/.
+cp -v $lib64/libtermcap*     $shared_lib/.
+cp -v $lib64/libz*           $shared_lib/.
+cp -v $lib64/libssl.so.10    $shared_lib/.
+cp -v $lib64/libcrypto.so.10 $shared_lib/.
+cp -v $lib64/libkrb5*        $shared_lib/.
+cp -v $lib64/libcom_err*     $shared_lib/.
+cp -v $lib64/libgssapi*      $shared_lib/.
+cp -v $lib64/libxslt*        $shared_lib/.
+cp -v $lib64/libldap*        $shared_lib/.
+cp -v $lib64/liblber*        $shared_lib/.
+cp -v $lib64/libsasl2*       $shared_lib/.
+cp -v $lib64/libuuid*        $shared_lib/.
+cp -v $lib64/libxml2.so.2    $shared_lib/.
+cp -v $lib64/libevent*       $shared_lib/.
+cp -v $lib64/libpython3.6m.so.1.0   $shared_lib/.
 
 rm -f $shared_lib/*.a
