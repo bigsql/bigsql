7d4f3ee6ac8ee684b3843cb20c6a42c0aee9afd4
denis lussier <denis@lussier.io>
2020-02-11 05:31:43 -0800

    wip

--- devel/pgbin/build/sharedLibs.sh
+++ devel/pgbin/build/sharedLibs.sh
@@ -15,35 +15,33 @@ else
   exit 1
 fi 
 
-echo "lib64=$lib64"
 shared_lib=/opt/pgbin-build/pgbin/shared/lib/
 mkdir -p $shared_lib
 rm -f $shared_lib/*
 
-cp -v /lib/`arch`-linux-gnu/liblzma.so.5        $shared_lib/.
-cp -v /lib/`arch`-linux-gnu/libz.so.1           $shared_lib/.
-cp -v /lib/`arch`-linux-gnu/libssl.so.1.0.0     $shared_lib/.
-cp -v /lib/`arch`-linux-gnu/libcrypto.so.1.0.0  $shared_lib/.
-cp -v /lib/`arch`-linux-gnu/libbsd.so.0         $shared_lib/.
-cp -v /lib/`arch`-linux-gnu/libc.so.6           $shared_lib/.
-cp -v /lib64/ld-linux-x86-64.so.2               $shared_lib/.
-#cp -v $lib64/libkrb5*        $shared_lib/.
-#cp -v $lib64/libcom_err*     $shared_lib/.
-#cp -v $lib64/libgssapi*      $shared_lib/.
-#cp -v $lib64/libldap*        $shared_lib/.
-cp -v $lib64/libedit.so.2        $shared_lib/.
-cp -v $lib64/*termcap*           $shared_lib/.
+##cp -v /lib/`arch`-linux-gnu/liblzma.so.5        $shared_lib/.
+##cp -v /lib/`arch`-linux-gnu/libz.so.1           $shared_lib/.
+##cp -v /lib/`arch`-linux-gnu/libssl.so.1.0.0     $shared_lib/.
+##cp -v /lib/`arch`-linux-gnu/libcrypto.so.1.0.0  $shared_lib/.
+##cp -v /lib/`arch`-linux-gnu/libbsd.so.0         $shared_lib/.
+##cp -v /lib/`arch`-linux-gnu/libc.so.6           $shared_lib/.
+##cp -v /lib64/ld-linux-x86-64.so.2               $shared_lib/.
+cp -v $lib64/libz*           $shared_lib/.
+cp -v $lib64/libssl*         $shared_lib/.
+cp -v $lib64/libcrypto*      $shared_lib/.
+cp -v $lib64/libkrb5*        $shared_lib/.
+cp -v $lib64/libgssapi*      $shared_lib/.
+cp -v $lib64/libldap*        $shared_lib/.
+cp -v $lib64/libedit*        $shared_lib/.
+cp -v $lib64/libxml2*        $shared_lib/.
 cp -v $lib64/libxslt*        $shared_lib/.
 cp -v $lib64/liblber*        $shared_lib/.
 cp -v $lib64/libsasl2*       $shared_lib/.
-#cp -v $lib64/libuuid*        $shared_lib/.
-cp -v $lib64/libxml2*        $shared_lib/.
 cp -v $lib64/libevent*       $shared_lib/.
-cp -v $lib64/libicuuc.so.55  $shared_lib/.
-cp -v $lib64/libicudata.so.55 $shared_lib/.
-cp -v $lib64/libstdc++.so.6   $shared_lib/.
-#cp -v $lib64/libuv*          $shared_lib/.
-#cp -v $lib64/libpython3.?m.so.1.0   $shared_lib/.
+cp -v $lib64/libicuuc*       $shared_lib/.
+cp -v $lib64/libicudata*      $shared_lib/.
+cp -v $lib64/libuv*          $shared_lib/.
+cp -v $lib64/libpython3.?m.so.1.0   $shared_lib/.
 #cp -v $lib64/libcassandra.so.2   $shared_lib/.
 
 rm -f $shared_lib/*.a
--- setupAWS.sh
+++ setupAWS.sh
@@ -1,81 +1,4 @@
-# BIGSQL-APG
 
-EMAIL="denis@lussier.io"
-NAME="denis lussier"
-git config --global user.email "$EMAIL"
-git config --global user.name "$NAME"
-git config --global push.default simple
-git config --global credential.helper store
-#mkdir -p ~/dev/
-#cd ~/dev
-#git clone https://github.com/bigsql/bigsql-apg.git
-
-if [ `uname` == 'Darwin' ]; then
-  owner_group="$USER:wheel"
-  brew install sqlite3 python3 curl wget
-  brew install gcc flex bison zlib readline libxml2 libxslt
-  brew install llvm libuv libevent pkg-config unixodbc
-else
-  owner_group="$USER:$USER"
-  APT="sudo apt -y"
-  $APT install sqlite3 python3 curl wget
-  $APT install openjdk-8-jdk build-essential flex bison zlib1g-dev \
-    libxml2-dev libxslt-dev libedit-dev libssl-dev chrpath \
-    libperl-dev libpython3-dev pkg-config libevent-dev cmake \
-    libcurl4-openssl-dev *unixodbc* clang lxc lxcfs
-fi
-
-sudo mkdir /opt/pgbin-build
-sudo chown $owner_group /opt/pgbin-build
-mkdir -p /opt/pgbin-build/pgbin/bin
-sudo mkdir /opt/pgcomponent
-sudo chown $owner_group /opt/pgcomponent
-mkdir -p ~/dev
-cd ~/dev
-mkdir in
-mkdir out
-mkdir history
-
-cd ~
-wget https://bootstrap.pypa.io/get-pip.py
-sudo python3 get-pip.py
-rm get-pip.py
-sudo pip3 install awscli
-mkdir -p ~/.aws
-cd ~/.aws
-touch config
-# vi config
-chmod 600 config
-
-## ENV for ~/.bashrc or ~/.bash_profile 
-alias git-push="cd ~/dev/bigsql-apg; git status; git add .; git commit -m wip; git push"
-alias bp="cd ~/dev/bigsql-apg; . ./bp.sh"
-alias http="cd ~/dev/bigsql-apg; ./startHTTP.sh"
-
-export REGION=us-west-2
-export BUCKET=s3://bigsql-apg-download
-
-export DEV=$HOME/dev
-export HIST=$DEV/history
-export IN=$DEV/in
-export SRC=$IN/sources
-export OUT=$DEV/out
-export APG=$DEV/bigsql-apg
-export VER=$APG/src/conf/versions.sql
-export BLD=/opt/pgbin-build/pgbin/bin
-
-export DEVEL=$APG/devel
-export PG=$DEVEL/pg
-export PGBIN=$DEVEL/pgbin
-export UTIL=$DEVEL/util
-export CLI=$APG/cli/scripts
-export PSX=$APG/out/posix
-export REPO=http://localhost:8000
-
-export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/jre/bin
-export PATH=$PATH:$JAVA_HOME/bin
-
-## OTHER setup after ~/.bashrc, ~/.aws & /opt/pgbin-build #######
 cd $IN
 cp $APG/devel/util/pull-s3.sh .
 ./pull-s3.sh
@@ -84,4 +7,3 @@ chmod 755 *.sh
 cd $BLD
 cp -p $APG/devel/pgbin/build/* .
 ./sharedLibs.sh
-./build-all-pgbin.sh 11
