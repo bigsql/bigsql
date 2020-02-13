942704900f914f9ab3b6cc256c10141c57336183
denis lussier <denis@lussier.io>
2020-02-13 14:28:52 -0800

    add missing libs & remove extra libs

--- devel/pgbin/build/sharedLibs.sh
+++ devel/pgbin/build/sharedLibs.sh
@@ -21,17 +21,23 @@ rm -f $shared_lib/*
 
 cp -v $lib64/libz*           $shared_lib/.
 cp -v $lib64/libssl*         $shared_lib/.
-cp -v $lib64/libcrypto*      $shared_lib/.
+cp -v $lib64/libcrypt.so.1   $shared_lib/.
+cp -v $lib64/libcrypto.so.10 $shared_lib/.
 cp -v $lib64/libkrb5*        $shared_lib/.
 cp -v $lib64/libgssapi*      $shared_lib/.
 cp -v $lib64/libldap*        $shared_lib/.
 cp -v $lib64/libedit*        $shared_lib/.
-cp -v $lib64/libxml2*        $shared_lib/.
+cp -v $lib64/libxml2.so.2    $shared_lib/.
 cp -v $lib64/libxslt.so*     $shared_lib/.
 cp -v $lib64/liblber*        $shared_lib/.
 cp -v $lib64/libsasl2*       $shared_lib/.
 cp -v $lib64/libevent*       $shared_lib/.
-cp -v $lib64/libpython3*so*  $shared_lib/.
+cp -v $lib64/libnss3*        $shared_lib/.
+cp -v $lib64/libnssutil3*    $shared_lib/.
+cp -v $lib64/libsmime*       $shared_lib/.
+cp -v $lib64/libplds4*       $shared_lib/.
+cp -v $lib64/libplc4*        $shared_lib/.
+cp -v $lib64/libpython3.6m.so.1.0 $shared_lib/.
 #cp -v $lib64/libcassandra.so.2   $shared_lib/.
 
 rm -f $shared_lib/*.a
