89ae5102f50f90e4d3a7165d3db80349a296019d
denis lussier <denis@lussier.io>
2020-02-16 05:04:12 -0800

    wip

--- www/comp.py
+++ www/comp.py
@@ -1,46 +1,23 @@
-import sqlite3, sys
+import sqlite3, sys, pgsql
 
 isSHOW_COMP_PLAT = 'N'
 BR = "<br>"
-WIDTH = 1100
+WIDTH = 900
 COL_SIZE = 190
-NUM_COLS = 5
+NUM_COLS = 4
 FONT_SIZE = 2
 IMG_SIZE = 25
 BORDER=0
 
 
 def print_top():
-  print('<center><table><tr><td> \n')
-
-  print('<table height=100 width=' + str(WIDTH) + ' border=0 bgcolor=black cellpadding=2> \n' +
-        '  <tr> \n' + \
-        '    <td><img width=325 src=img/pgsql-banner.png /></td> \n' + \
-        '    <td width=375><center><font color=whitesmoke>\n' + \
-        '     Linux on ARM & AMD plus OSX & Windows<br>&nbsp;<br>\n' + \
-        '      <a href=""><font color=whitesmoke>FAQ</font></a>&nbsp;&nbsp;&nbsp;\n' + \
-        '      <a href=""><font color=whitesmoke>About Us</font></a>\n' + \
-        '    </font></center></td>\n' + \
-        '    <td width=400><font size=-1 color=whitesmoke>\n' + \
-        '<center><b>Installation Instructions:</b></center>\n' + \
-        '<code>BUCKET=https://bigsql-apg-download.s3.amazonaws.com<br>\n' + \
-        'INSTALLER=$BUCKET/REPO/install.py<br>\n' + \
-        'python -c "$(curl -fsSL $INSTALLER)</code>"\n' + \
-        '<br>&nbsp;<br>\n' + \
-        '<center><b>Usage Instructions:</b></center>\n' + \
-        '<code>cd bigsql; ./apg info; ./apg list; ./apg help;\n' + \
-        './apg install pg11; ./apg start pg11;\n'
-        './apg install all-extensions</code>' + \
-        '    </td>\n' + \
-        '  </tr>\n' + \
-        '</table>\n\n')
-
+  pgsql.print_header()
   print('<table border=' + str(BORDER) + ' bgcolor=white cellpadding=2>')
 
  
 def print_bottom():
   print('    </td>\n  </tr>\n</table>\n</center>\n\n<br>\n')
-  print('&copy; BigSQL 2012-2020, All rights reserved.')
+  pgsql.print_footer()
 
 
 def get_columns(d):
---
+++ www/pgsql.py
@@ -0,0 +1,29 @@
+
+def print_header():
+  print('<center><table><tr><td> \n')
+
+  print('<table height=100 width=900 border=0 bgcolor=black cellpadding=2> \n' +
+        '  <tr> \n' + \
+        '    <td><img width=325 src=img/pgsql-banner.png /></td> \n' + \
+        '    <td width=375><center><font color=whitesmoke>\n' + \
+        '     Linux on ARM & AMD plus OSX & Windows<br>&nbsp;<br>\n' + \
+        '      <a href=""><font color=whitesmoke>FAQ</font></a>&nbsp;&nbsp;&nbsp;\n' + \
+        '      <a href=""><font color=whitesmoke>About Us</font></a>\n' + \
+        '    </font></center></td>\n' + \
+        '    <td width=400><font size=-1 color=whitesmoke>\n' + \
+        '<center><b>Installation Instructions:</b></center>\n' + \
+        '<code>BUCKET=https://bigsql-apg-download.s3.amazonaws.com<br>\n' + \
+        'INSTALLER=$BUCKET/REPO/install.py<br>\n' + \
+        'python -c "$(curl -fsSL $INSTALLER)</code>"\n' + \
+        '<br>&nbsp;<br>\n' + \
+        '<center><b>Usage Instructions:</b></center>\n' + \
+        '<code>cd bigsql; ./apg info; ./apg list; ./apg help;\n' + \
+        './apg install pg11; ./apg start pg11;\n'
+        './apg install all-extensions</code>' + \
+        '    </td>\n' + \
+        '  </tr>\n' + \
+        '</table>\n\n')
+
+ 
+def print_footer():
+  print('&copy; BigSQL 2012-2020, All rights reserved.')
