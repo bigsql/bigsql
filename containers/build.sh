f31ee222934b2a1875edff971f0878e6ea10297e
denis lussier <denis@lussier.io>
2020-02-16 04:49:43 -0800

    wip

--- containers/build.sh
+++ containers/build.sh
@@ -2,5 +2,5 @@
 osVersion=$1
 baseImage=$2
 
-sudo docker image rm -f $baseImage
-sudo docker build --file Dockerfile.$osVersion --tag $baseImage .
+docker image rm -f $baseImage
+docker build --file Dockerfile.$osVersion --tag $baseImage .
