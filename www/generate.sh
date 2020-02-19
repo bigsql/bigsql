06a2c7fe0a5b511ef4b35fd415d4fb7b1335dd9d
Denis Lussier <denis@lussier.io>
2020-02-19 12:45:00 -0800

    wip

--- www/generate.sh
+++ www/generate.sh
@@ -1,7 +1,7 @@
 COMP=components
 
-sqlite3 local.db < $APG/src/conf/components.sql
-sqlite3 local.db < $APG/src/conf/versions.sql
+sqlite3 local.db < $IO/src/conf/components.sql
+sqlite3 local.db < $IO/src/conf/versions.sql
 
 python3 $COMP.py > html/$COMP.html
 
