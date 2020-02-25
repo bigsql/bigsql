c0f3c306dfbc97522c5d9ac9dfb541e712d024f7
denis lussier <denis@lussier.io>
2020-02-25 15:33:50 -0800

    wip

--- src/postgis-pgXX/remove-postgis-pgXX.py
+++ src/postgis-pgXX/remove-postgis-pgXX.py
@@ -5,5 +5,10 @@
 
 import util
 
-util.remove_pgconf_keyval("postgis", "shared_preload_libraries", "postgis")
+ver = "pgXX"
+ext = "postgis-2.5"
+if ver == "pg12":
+  ext = "postgis-3"
+
+util.remove_pgconf_keyval(ver, "shared_preload_libraries", ext)
 
