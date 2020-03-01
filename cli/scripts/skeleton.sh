d2413f8dd5f853b072565b6ed683d6900c6b4d9b
Lussier <denisl@oscg-partners.com>
2020-03-01 05:46:48 -0800

    wip

--- cli/scripts/skeleton.sh
+++ cli/scripts/skeleton.sh
@@ -1,30 +1,34 @@
 
 function testAll {
-  ./io install cron-pg$1          -d demo
-  ./io install tdsfdw-pg$1        -d demo
-  ./io install oraclefdw-pg$1     -d demo
-  ./io install mysqlfdw-pg$1      -d demo
-  ./io install postgis-pg$1       -d demo
+  ./io install cron-pg$1
   ./io install repack-pg$1        -d demo
   ./io install http-pg$1          -d demo
   ./io install plprofiler-pg$1    -d demo
   ./io install hypopg-pg$1        -d demo
   ./io install orafce-pg$1        -d demo
   ./io install pglogical-pg$1     -d demo
-  ./io install bulkload-pg$1      -d demo
   ./io install partman-pg$1       -d demo
   ./io install audit-pg$1         -d demo
   ./io install ddlx-pg$1          -d demo
   ./io install anon-pg$1          -d demo
-  ##./io install hive_fdw-pg$1    -d demo
-  ##./io install cassandra_fdw-pg$1 -d demo
+  if [ ! `uname` == "Darwin" ]; then
+    ./io install bulkload-pg$1    -d demo
+    ./io install tdsfdw-pg$1      -d demo
+    ./io install oraclefdw-pg$1   -d demo
+    ./io install mysqlfdw-pg$1    -d demo
+    ./io install postgis-pg$1     -d demo
+    ##./io install hive_fdw-pg$1  -d demo
+    ##./io install cassandra_fdw-pg$1 -d demo
+  fi
 }
 
 function test11 {
   ./io install pg11; 
   ./io start pg11 -y -d demo; 
-  ./io install timescaledb-pg11   -d demo
   ./io install pgtsql-pg11        -d demo
+  if [ ! `uname` == "Darwin" ]; then
+    ./io install timescaledb-pg11   -d demo
+  fi
 }
 
 function test12 {
