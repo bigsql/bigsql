5075a937f7e912bfc601c749b626df6ad5e6dedb
denis lussier <denis@lussier.io>
2020-03-01 05:14:52 -0800

    wip

--- devel/util/copy-all.sh
+++ devel/util/copy-all.sh
@@ -1,9 +1,10 @@
-outD=out-20200212
+outD=out-20200301
 
 rm -rf history/$outD
 mkdir history/$outD
 
 cp -p $OUT/* history/$outD/.
-rm history/$outD/bigsql-1*
+rm history/$outD/pgsql-9*
+rm history/$outD/pgsql-1*
 
 ./copy-to-s3.sh $outD
