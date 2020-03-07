76706301afe4373e94d6c92518a0d46a4c40881c
Denis Lussier <denis@lussier.io>
2020-03-06 16:01:03 -0800

    wip

--- www/pgsql.py
+++ www/pgsql.py
@@ -12,21 +12,21 @@ def print_header(pWidth):
 
   print("<table width=" + str(pWidth) + " cellpadding=2 >")
 
-  print("<tr><td><b>Tested with</b></td><td>")
-  print("CentOS/RHEL 7+,&nbsp; Amazon Linux 2,&nbsp; Ubuntu 16+, &nbsp; " + \
+  print("<tr><td align=right><b>Tested with:</b></td><td>")
+  print("Amazon Linux 2, CentOS/RHEL 7+,&nbsp; Ubuntu 16+, &nbsp; " + \
         "and OSX 10.13+")
   print("</td></tr>")
 
   print("<tr><td></td></tr>")
 
-  print("<tr><td><b>Install cmd</b></td><td>")
-  print('<code>python3 -c "$(curl -fsSL https://pgsql-io-download.s3.amazonaws.com/REPO/install.py)"</code>')
+  print("<tr><td align=right><b>Install command line:</b></td><td>")
+  print('<textarea rows=1 cols=100>python3 -c "$(curl -fsSL https://pgsql-io-download.s3.amazonaws.com/REPO/install.py)"</textarea>')
   print("</td></tr>")
   
   print("<tr><td></td></tr>")
 
-  print("<tr><td><b>Usage sample</b></td><td>")
-  print('<code>cd pgsql; ./io install pg11; ./io start pg11; ./io install timescaledb-pg11</code>') 
+  print("<tr><td align=right><b>Usage sample:</b></td><td>")
+  print('<textarea rows=1 cols=100>cd pgsql; ./io install pg11; ./io start pg11; ./io install timescaledb-pg11</textarea>') 
   print("</td></tr>")
 
   print("</table>")
