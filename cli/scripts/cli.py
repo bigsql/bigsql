0a980e7331cc9a60c1de3a8d5d78d2991f53a44c
denis lussier <denis@lussier.io>
2020-03-16 16:28:50 -0700

    fix regression in update hub process

--- cli/scripts/cli.py
+++ cli/scripts/cli.py
@@ -626,9 +626,9 @@ def unpack_comp(p_app, p_old_ver, p_new_ver):
       my_logger.info(msg)
       if p_app in ('hub'):
         if util.get_platform() == "Windows":
-          copy2(os.path.join(MY_HOME, "cli.bat"), backup_target_dir)
+          copy2(os.path.join(MY_HOME, "io.bat"), backup_target_dir)
         else:
-          copy2(os.path.join(MY_HOME, "cli"), backup_target_dir)
+          copy2(os.path.join(MY_HOME, "io"), backup_target_dir)
         os.rename(new_comp_dir, "hub_new")
         ## run the update_hub script in the _new directory
         upd_hub_cmd = sys.executable + " hub_new" + os.sep + "hub" + os.sep + "scripts" + os.sep + "update_hub.py "
