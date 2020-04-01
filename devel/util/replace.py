771906b8e20789a22413f76d06ebe0277a77c231
denis lussier <denis@lussier.io>
2020-03-31 23:51:30 -0700

    wip

--- devel/util/replace.py
+++ devel/util/replace.py
@@ -2,19 +2,18 @@ import os
 
 for subdir, dirs, files in os.walk('./'):
   for orig_file in files:
-    if orig_file.startswith("pgspock_") and (orig_file.endswith(".c") or orig_file.endswith(".h")):
+    if orig_file.startswith("spock_") and (orig_file.endswith(".c") or orig_file.endswith(".h")):
       full_file = subdir + "/" + orig_file
       print(full_file)
       with open (full_file, 'r') as source_content:
         src = source_content.read()
-        src = src.replace("pglogical", "pgspock")
-        src = src.replace("PGLogical", "PGSpock")
-        src = src.replace("Copyright", "Portions Copyright")
-        src = src.replace("PGLOGICAL", "PGSPOCK")
-        src = src.replace("PGL", "PGS")
-        src = src.replace("PG_LOGICAL", "PG_SPOCK")
-        src = src.replace("PGlogical", "PGspock")
-        src = src.replace("pgl_", "pgs_")
+        src = src.replace("pglogical", "spock")
+        src = src.replace("PGLogical", "Spock")
+        src = src.replace("PGLOGICAL", "SPOCK")
+        src = src.replace("PGL", "SPK")
+        src = src.replace("PG_LOGICAL", "SPOCK")
+        src = src.replace("PGlogical", "Spock")
+        src = src.replace("pgl_", "spk_")
         with open (full_file, 'w') as f:
           f.write(src)
         #print(src)
