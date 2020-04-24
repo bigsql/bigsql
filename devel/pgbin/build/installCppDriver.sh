9a1256470852e0ca419a4c3a09f788f863a85404
denis lussier <denis@lussier.io>
2020-04-24 16:57:35 -0700

    wip

--- devel/pgbin/build/installCppDriver.sh
+++ devel/pgbin/build/installCppDriver.sh
@@ -1,8 +1,18 @@
-uvV=1.34.0
-cpdV=2.15.0
+cpdV=2.15.1
 
-rm -rf *.deb*
-wget https://downloads.datastax.com/cpp-driver/ubuntu/16.04/dependencies/libuv/v$uvV/libuv1-dev_$uvV-1_amd64.deb
-wget https://downloads.datastax.com/cpp-driver/ubuntu/16.04/cassandra/v$cpdV/cassandra-cpp-driver-dev_$cpdV-1_amd64.deb
+rm -rf *.rpm*
+cas=https://downloads.datastax.com/cpp-driver/centos/7/cassandra
+
+rpm=cassandra-cpp-driver-$cpdV-1.el7.x86_64.rpm
+wget $cas/v$cpdV/$rpm
+sudo yum install -y $rpm
+
+
+rpm=cassandra-cpp-driver-devel-$cpdV-1.el7.x86_64.rpm
+wget $cas/v$cpdV/$rpm
+sudo yum install -y $rpm
+
+rm *.rpm
+
 
 
