08d0c783c609428f53758bfca9db62f9f2195f01
denis lussier <denis@lussier.io>
2020-05-03 22:02:57 -0700

    wip cleanup

--- website/chalice/deploy.sh
+++
@@ -1,3 +0,0 @@
-sudo cp -v html/components.html /var/www/html/index.html
-sudo cp -r html/img /var/www/html/
-
--- website/chalice/generate.sh
+++
@@ -1,7 +0,0 @@
-COMP=components
-
-sqlite3 local.db < $IO/src/conf/components.sql
-sqlite3 local.db < $IO/src/conf/versions.sql
-
-python $COMP.py > html/$COMP.html
-
